88890fa1f7bf40959252d80c49c78b38103906ed
Pauline Bravo <bravo.pauline@gmail.com>
2018-01-30 16:03:54 +0100

    display an array with jstl

--- WebContent/WEB-INF/bonjour.jsp
+++ WebContent/WEB-INF/bonjour.jsp
@@ -1,12 +1,19 @@
 <%@ page pageEncoding="UTF-8" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8" />
-<title>Test</title>
-</head>
-<body>
-<p>Bonjour ${!empty name ? name : ''}</p>
-<p>${ noms[2] }</p>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Test</title>
+    </head>
+    <body>
+      
+      <c:forEach items="${titres}" var="titre">
+    <p> <c:out value="${ titre }" /> !</p>
+    
+    
+</c:forEach>
+
+    </body>
 </html>
--- src/com/mytest/servlets/Test.java
+++ src/com/mytest/servlets/Test.java
@@ -9,17 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mytest.beans.Auteur;
+
 
 @WebServlet("/Test")
 public class Test extends HttpServlet {
     private static final long serialVersionUID = 1L;
-       
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        request.setAttribute("name", name);
-        String[] noms = {"coquillette", "chouquette", "cacahuette"};
-        request.setAttribute("noms", noms);;
+       String [] titres = {"Des yeux de soie" , " Un peu de soleil dans l'eau froide", " Un certain sourire"};
+       request.setAttribute("titres", titres);
+        
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
 
