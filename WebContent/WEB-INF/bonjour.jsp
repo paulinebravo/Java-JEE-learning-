cd1cb8e3d08cc712f282485da6f6a4bad21d1219
Pauline Bravo <bravo.pauline@gmail.com>
2018-02-08 14:50:10 +0100

    cookies

--- WebContent/WEB-INF/bonjour.jsp
+++ WebContent/WEB-INF/bonjour.jsp
@@ -1,25 +1,25 @@
 <%@ page pageEncoding="UTF-8" %>
-
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Test</title>
-    </head>
-    <body>
-      
-     
-    <c:if test="${ !empty nom }"><p><c:out value="Bonjour, vous vous appelez ${ nom }" /></p></c:if>
-        
-        <form method="post" action="bonjour">
+<head>
+<meta charset="utf-8" />
+<title>Test</title>
+</head>
+<body>
+    <c:out value="${ prenom }" />
+    <form method="post" action="bonjour">
+        <p>
             <label for="nom">Nom : </label>
             <input type="text" name="nom" id="nom" />
-            
-            <input type="submit" />
-        </form>
+        </p>
+        <p>
+            <label for="prenom">Prénom : </label>
+            <input type="text" name="prenom" id="prenom" />
+        </p>
+        
+        <input type="submit" />
+    </form>
     
-
-
-    </body>
+</body>
 </html>
--- src/com/mytest/servlets/Test.java
+++ src/com/mytest/servlets/Test.java
@@ -1,34 +1,49 @@
 package com.mytest.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import com.mytest.beans.Auteur;
-
-
+/**
+ * Servlet implementation class Test
+ */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      
-    this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    
+       
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("prenom")) {
+                    request.setAttribute("prenom", cookie.getValue());
+                }
+            }
+        }
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
         
-        request.setAttribute("nom", nom);
+        Cookie cookie = new Cookie("prenom", prenom);
+        cookie.setMaxAge(60 * 60 * 24 * 30);
+        response.addCookie(cookie);
         
         this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
-    
+
 }
